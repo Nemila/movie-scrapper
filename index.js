1ead316e4e3a732f41916a3a658ac3564a687a7d
Lamine <pridila.2006@gmail.com>
2023-06-30 23:57:59 +0000

    movies-api

---
+++ index.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const express = require("express");
+// const app = express();
+
+async function getHrefs() {
+  try {
+    // Step 1: Fetch the main URL and get hrefs from .mov elements
+    process.stdout.write(`\nFetching PAGE ${1} URL...\n`);
+    const mainResponse = await axios.get(
+      `https://wiflix.surf/film-en-streaming/page/${1}/`
+    );
+    const $ = cheerio.load(mainResponse.data);
+
+    const movieData = $("a.mov-t")
+      .map((_, element) => ({
+        title: $(element).text(),
+        url: $(element).attr("href"),
+      }))
+      .get();
+    process.stdout.write(
+      `Step 1 completed. Total movies: ${movieData.length}\n`
+    );
+
+    // Step 2: Navigate to each href and get hrefs from .linkstab elements
+    const movieEmbed = [];
+    let counter = 0;
+    for (const movie of movieData) {
+      counter++;
+      process.stdout.write(`\rProcessing movie ${counter}/${movieData.length}`);
+      const response = await axios.get(movie.url);
+      const html = response.data;
+      const inner$ = cheerio.load(html);
+      const innerHrefs = inner$(".linkstab a")
+        .map((_, element) => inner$(element).attr("href").split("?u=")[1])
+        .get();
+      movieEmbed.push({ ...movie, embed: innerHrefs });
+    }
+    process.stdout.write(`\nStep 2 completed. Movies processed: ${counter}`);
+
+    // Step 3: Log the resulting array
+    fs.writeFile("./movies.json", JSON.stringify(movieEmbed), (err) => {
+      if (err) console.error(err);
+      process.stdout.write("File updated with success!");
+    });
+    return movieEmbed;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+getHrefs();
+
+// app.get("/page", async (req, res) => {
+//   //   const data = await getHrefs(req.);
+//   res.json(data);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("BIENVENUE SUR FILM-FR | API FAIT PAR LAMINE DIAMOUTENE");
+// });
+
+// app.listen(3000);
