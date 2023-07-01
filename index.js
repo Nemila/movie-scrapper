ccaafa57fe7fffb19520382167d4e51b7e496a48
Lamine <pridila.2006@gmail.com>
2023-07-01 06:18:39 +0000

    add embed urls

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const app = require("express")();
 const movies = require("./utils/movies");
 
 app.get("/", (req, res) => {
-  res.json({
-    title: "Bienvenue sur filmFr",
-    message: "Faire par lamine diamoutene",
+  res.status(200).json({
+    title: "Bienvenue sur FilmFR!",
+    message: "Fait par Lamine Diamoutene.",
   });
 });
 
 app.get("/films", async (req, res) => {
   const { page } = req.query;
   const data = page ? await movies.getMovies(page) : await movies.getMovies(1);
+  res.status(200).json(data);
+});
+
+app.get("/films/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await movies.getEmbedUrls(id);
   res.status(200).send(data);
 });
 
--- utils/movies.js
+++ utils/movies.js
@@ -8,14 +8,36 @@ const getMovies = async (page) => {
   $ = cheerio.load(data);
 
   const movieData = $("a.mov-t")
-    .map((_, element) => ({
-      title: $(element).text(),
-      url: $(element).attr("href"),
-    }))
+    .map((_, element) => {
+      const url = $(element).attr("href");
+      var start = url.lastIndexOf("/") + 1;
+      var end = url.lastIndexOf(".html");
+
+      return {
+        id:
+          start !== -1 && end !== -1 && start < end
+            ? url.substring(start, end)
+            : url,
+        title: $(element).text(),
+      };
+    })
     .get();
 
   return movieData;
 };
 
-const movies = { getMovies };
+const getEmbedUrls = async (id) => {
+  const { data } = await axios.get(
+    `https://wiflix.surf/film-en-streaming/${id}.html`
+  );
+  $ = cheerio.load(data);
+
+  const embedUrls = $(".tabs-sel > a")
+    .map((_, element) => $(element).attr("href").split("?u=")[1])
+    .get();
+
+  return embedUrls;
+};
+
+const movies = { getMovies, getEmbedUrls };
 module.exports = movies;
